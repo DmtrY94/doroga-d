755b40ae771785f4d2e144bf67253c72a5f1da8c
DmtrY94 <dmitriyyolov94@gmail.com>
2021-04-05 20:30:49 +0300

    adding files

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
     linkActiveClass: 'header-nav__item-active',
   },
   // Target (https://go.nuxtjs.dev/config-target)
-  target: 'static',
+  ssr: false, 
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
