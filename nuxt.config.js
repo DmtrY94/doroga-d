0c2439825ced9eb146eac4d45e9cedbf2686edb6
DmtrY94 <63665380+DmtrY94@users.noreply.github.com>
2021-04-05 20:55:06 +0300

    Add files via upload

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,6 +7,7 @@ export default {
   },
   // Target (https://go.nuxtjs.dev/config-target)
   ssr: false, 
+  target: "static",
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
